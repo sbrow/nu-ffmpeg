555073c4283a510ce10cef5b01ee7e0a8c44a093
Spencer Brower <brower.spencer@gmail.com>
2024-01-08 13:35:14 -0500

    feat: Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+#
+
+```nu
+use ./ffprobe
---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = with pkgs; [
+          ffmpeg
+
+          sbcl
+
+          nushellFull
+        ];
+      };
+    };
+}
